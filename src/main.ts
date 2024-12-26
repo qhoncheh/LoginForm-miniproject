a4dca3b13800af480a455e9898b4bea437c12561
ghonche <ataee.ghonche@gmail.com>
2024-12-26 17:48:09 +0330

    some edit 😉

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,43 @@
 import { v4 as uuidv4 } from 'uuid'
-import './index.css'
+import '../style.css'
 
+type Item = {
+    id: string,
+    name:string,
+    email:string
+}
 
 const form = document.querySelector<HTMLFormElement>('#add-user-form')
 
 const name = document.querySelector<HTMLInputElement>('#name')
 const email = document.querySelector<HTMLInputElement>('#email')
 
-const users = document.querySelector<HTMLDivElement>('user-list')
+const users = document.querySelector<HTMLDivElement>('#user-list')
 
 form?.addEventListener('submit', e => {
     e.preventDefault()
 
-    if(name?.value === undefined || email?.value === undefined) return
+    if (name?.value === undefined || email?.value === undefined) return
 
-    const userItem = {
-        id:uuidv4(),
+    const userItem: Item = {
+        id: uuidv4(),
         name:name.value,
         email:email.value,
     } 
+    addUser(userItem)
 })
 
+function addUser(item: Item){
+    const container = document.createElement('div')
 
-function addUser(item){
-    
+    const nameElement = document.createElement('p')
+    const emailElement = document.createElement('p')
+
+    nameElement.append(item.name)
+    emailElement.append(item.email)
+
+    container.append(nameElement, emailElement)
+
+    users?.append(container)
+
 }
