85a6e77df9dd9c64ff71540e4defd15cec0aabbb
ghonche <ataee.ghonche@gmail.com>
2024-12-26 17:04:16 +0330

    add some code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-import "./index.css"
+import { v4 as uuidv4 } from 'uuid'
+import './index.css'
+
+
+const form = document.querySelector<HTMLFormElement>('#add-user-form')
+
+const name = document.querySelector<HTMLInputElement>('#name')
+const email = document.querySelector<HTMLInputElement>('#email')
+
+const users = document.querySelector<HTMLDivElement>('user-list')
+
+form?.addEventListener('submit', e => {
+    e.preventDefault()
+
+    if(name?.value === undefined || email?.value === undefined) return
+
+    const userItem = {
+        id:uuidv4(),
+        name:name.value,
+        email:email.value,
+    } 
+})
+
+
+function addUser(item){
+    
+}
